053e4519730313fbb6771f8178075b323269a66e
jorgeaguiar <jorge.aguiar@waveapps.com.ar>
2025-11-28 11:57:06 -0300

    comit cero

--- app/src/main/java/com/waveapp/tarotgemini/MainActivity.kt
+++ app/src/main/java/com/waveapp/tarotgemini/MainActivity.kt
@@ -1,3 +1,4 @@
+// kotlin
 package com.waveapp.tarotgemini
 
 import android.os.Bundle
@@ -5,12 +6,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.waveapp.tarotgemini.ui.theme.TarotGeminiTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +17,13 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TarotGeminiTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    // Aquí irá el contenido de la app
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TarotGeminiTheme {
-        Greeting("Android")
     }
 }
